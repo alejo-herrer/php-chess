db6d0141824de9dc4c730d819f0e82a0347d6fd9
AlejoHerrer <alejandroherreralbengrin@gmail.com>
2026-04-05 11:22:30 -0500

    Implementación de renderizado de las fichas 2.0

---
+++ partida.js
@@ -0,0 +1,18 @@
+class Partida {
+
+    constructor(jugadorNeg,jugadorBlan,numeroPart){
+        this.jugadorNeg = jugadorNeg;
+        this.jugadorBlan = jugadorBlan;
+        this.numeroPart = numeroPart;
+        this.contTurnos = [];
+
+    }
+
+    turno(contTurnos){
+
+        
+
+
+    }
+
+}
--- tablero.js
+++ tablero.js
@@ -5,9 +5,7 @@ export function mapearCoordenadas(){
     for(var i = 0 ; i<8 ; i++){
         tab[i] = [];
         for(var v = 0 ; v<8 ; v++){
-            tab[i][v] = document.getElementById(`cel-${i}-${v}`);
-            //console.log(tab[i][v].getAttribute("data-x"));
-            //console.log(tab[i][v].getAttribute("data-y"));
+            tab[i][v] = null;
         }
     }
 
